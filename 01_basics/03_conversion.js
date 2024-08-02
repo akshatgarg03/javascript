b532fa91ff621a5fa07a393575369a5ee18427cd
akshatgarg03 <148125278+akshatgarg03@users.noreply.github.com>
2024-08-02 19:25:50 +0530

    Squashed commit of the following:
    commit 85bd56f533f6dda2370282d2713842cd3358a129
    Author: akshatgarg03 <148125278+akshatgarg03@users.noreply.github.com>
    Date:   Fri Aug 2 19:19:46 2024 +0530
    
        commit
    
    commit 91cfd4d67b28af0f337151d1d004ae174c1d972a
    Merge: 1b4cd40 4758720
    Author: akshatgarg03 <148125278+akshatgarg03@users.noreply.github.com>
    Date:   Fri Aug 2 18:53:32 2024 +0530
    
        Merge pull request #2 from akshatgarg03/main
    
        Merge pull request #1 from akshatgarg03/01_basics

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,49 @@
+let score = undefined
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
+
+
+//"33"= 33
+//"33abc"=NaN
+//true=1;false=0
+
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1=true;0=false
+//""=false
+//"akshat"=true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+
+
+
+//*****************************operations**********************
+
+
+let str1 = "hello"
+let str2 = "akshat"
+let str3 = str1+str2
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log((3+4)*5%3);
+
