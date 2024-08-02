1b4cd40bf97eaa0d41bdebbe1d3771f939ac7af2
akshatgarg03 <148125278+akshatgarg03@users.noreply.github.com>
2024-08-02 18:24:19 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,26 @@
+const accountId = 144553
+let accountEmail = "askhat@gogle.com"
+var accountpassword = "12345"
+accountcity = "jabalpur"
+let accountState;
+
+
+
+//accountId = 2        not allowed
+
+
+console.log(accountId);
+
+accountEmail = "hv@hc.com"
+accountpassword= "212121"
+accountcity="udupi"
+
+/*
+
+prefer not to use var bcz of issues in block scope and functional scope
+*/
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountpassword,accountcity,accountState])
+ 
